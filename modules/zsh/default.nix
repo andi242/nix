cbed1eaad652b361065cca70c6eeff373f05c903
andi242 <andi242@gmail.com>
2025-01-29 21:12:15 +0000

    disable err

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -3,6 +3,9 @@ let
   aliases = {
     vi = "nvim";
     d1 = "du -h -d1";
+    rebuild = "sudo nixos-rebuild switch --flake .";
+    ngc = "nix-env --delete-generations old";
+    cleanup = "nix-store --gc";
   };
 in 
 {
@@ -10,12 +13,12 @@ in
     enable = true;
     shellAliases = aliases;
     enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-    defaultKeymap = "emacs";
-    historySubstringSearch = {
-      enable = false;
-    };
+    # autosuggestion.enable = true;
+    # syntaxHighlighting.enable = true;
+    # defaultKeymap = "emacs";
+    # historySubstringSearch = {
+    #   enable = false;
+    # };
     
-  }
+  };
 }
