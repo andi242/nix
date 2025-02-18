29adbf0462c0146dd6e2483e258ed3d5e89f1b04
andi242 <andi242@gmail.com>
2025-02-18 17:05:40 +0100

    zellfon

--- home-manager/modules/zsh/zellij.nix
+++ home-manager/modules/zsh/zellij.nix
@@ -8,8 +8,25 @@
       enableZshIntegration = false;
     };
   };
-  xdg.configFile."zellij" = {
-    source = ./zellij-config;
-    recursive = true; # because we inject zellij plugins as well
+  xdg = {
+    configFile."zellij" = {
+      source = ./zellij-config;
+      recursive = true; # because we inject zellij plugins as well
+    };
+    configFile."zellij/icon.png".source = ./zellij-config/icon.png;
+  };
+  xdg.desktopEntries = {
+    zellij = {
+      name = "Zellij";
+      exec = "kitty zellij";
+      terminal = false;
+      categories = [ "Application" ];
+      icon = "/home/ad/.config/zellij/icon.png"; #"/home/ad/.local/share/applications/zellij-icon.png";
+      actions = {
+        kitty = {
+          exec = "kitty";
+        };
+      };
+    };
   };
 }
