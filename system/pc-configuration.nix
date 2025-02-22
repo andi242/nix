eeca96ea44596ed918e3893da90212fd2e578939
andi242 <andi242@gmail.com>
2025-02-22 06:37:11 +0100

    pulse tests

--- system/pc-configuration.nix
+++ system/pc-configuration.nix
@@ -15,21 +15,34 @@
   networking.hostName = "nixos-pc";
   networking.networkmanager.enable = true;
 
-  nixpkgs.config.allowUnfree = true;
 
   time.timeZone = "Europe/Berlin";
 
   security.rtkit.enable = true;
 
-  # disable pipewire/wireplumber crap
-  services.pipewire = {
-    enable = lib.mkForce false;
-    # pulse.enable = false;
+  nixpkgs.config = {
+    allowUnfree = true; # allow unfree packages
+    pulseaudio = true;
   };
-  hardware.pulseaudio.enable = true;
-  hardware.pulseaudio.support32Bit = true;
-  nixpkgs.config.pulseaudio = true;
-  # hardware.pulseaudio.extraConfig = "load-module module-combine-sink";
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = false;
+    jack.enable = true; ### Optional, but awesome to have
+  };
+  # hardware.pulseaudio.enable = false;
+  # https://discourse.nixos.org/t/pipewire-without-pulseaudio/58801/4
+  # disable pipewire/wireplumber crap
+  # nixpkgs.config.allowUnfree = true;
+  # services.pipewire = {
+  #   enable = lib.mkForce false;
+  #   # pulse.enable = false;
+  # };
+  # hardware.pulseaudio.enable = true;
+  # hardware.pulseaudio.support32Bit = true;
+  # nixpkgs.config.pulseaudio = true;
+  # # hardware.pulseaudio.extraConfig = "load-module module-combine-sink";
 
   system.stateVersion = "24.11";
 }
