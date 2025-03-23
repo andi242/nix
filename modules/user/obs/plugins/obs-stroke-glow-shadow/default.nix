8e69696d58638a691e15b7c0708e37f3607640d8
andi242 <andi242@gmail.com>
2025-03-23 12:13:45 +0100

    plugin update

--- modules/user/obs/plugins/obs-stroke-glow-shadow/default.nix
+++ modules/user/obs/plugins/obs-stroke-glow-shadow/default.nix
@@ -3,34 +3,14 @@
 
 stdenv.mkDerivation rec {
   pname = "obs-stroke-glow-shadow";
-  version = "1.5.0";
+  version = "1.5.2";
   projUrl = "https://github.com/FiniteSingularity/obs-stroke-glow-shadow";
-  #
-  # src = fetchurl {
-  #   url = "${projUrl}/releases/download/v${version}/obs-stroke-glow-shadow-${version}-ubuntu-22.04.tar.gz.zip";
-  #   hash = "sha256-QPbuIPQaCAOKHCSWEEMaspE7DWY+StgRzDPakEPuZ94="; # ubuntu 22.04 hash 
-  # };
-  # dontBuild = true;
-  # nativeBuildInputs = [
-  #   gzip
-  # ];
-  # unpackPhase = ''
-  #   rm -rf "$out/share"
-  #   STRIPZIP=$(basename ${src} .zip) 
-  #   cp ${src} $STRIPZIP # some weird .zip extension attached?
-  #   gzip -d $STRIPZIP # get the .tar
-  #   tar xf *.tar # extract the .tar
-  #   mkdir -p "$out/lib/obs-plugins" # content of bin/64bits go here
-  #   mkdir -p "$out/share/obs/obs-plugins/${pname}" # content of ./data go here
-  #   mv ${pname}/bin/64bit/* "$out/lib/obs-plugins/"
-  #   mv ${pname}/data/* "$out/share/obs/obs-plugins/${pname}"
-  # '';
 
   src = fetchFromGitHub {
     owner = "FiniteSingularity";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-OwLMAX7zIJoDfSvbRRM5em0bLo3c5X2FYYEk8wCOUvs=";
+    hash = "sha256-+2hb4u+6UG7IV9pAvPjp4wvDYhYnxe98U5QQjUcdD/k=";
   };
   buildInputs = [
     obs-studio
