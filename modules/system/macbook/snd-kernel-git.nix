9ff6082fc4d2a0f4bc5b3ffa22e3f165a1481f40
andi242 <andi242@gmail.com>
2025-03-29 17:16:41 +0100

    fix mkdir

--- modules/system/macbook/snd-kernel-git.nix
+++ modules/system/macbook/snd-kernel-git.nix
@@ -41,7 +41,7 @@ stdenv.mkDerivation rec {
     make -C ${kernel.dev}/lib/modules/${kernel.modDirVersion}/build modules "M=$(pwd -P)/build/hda"
   '';
   installPhase = ''
-    mkdir $out/lib/modules/${kernel.modDirVersion}
+    mkdir -p $out/lib/modules/${kernel.modDirVersion}
     cp build/hda/snd-hda-codec-cirrus.ko $out/lib/modules/${kernel.modDirVersion}/
   '';
 }
