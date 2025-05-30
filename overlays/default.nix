49023c264e94ae5a437eaef724570445b93b42cf
andi242 <andi242@gmail.com>
2025-05-30 17:06:57 +0200

    updates

--- overlays/default.nix
+++ overlays/default.nix
@@ -8,11 +8,11 @@ in
   nixpkgs.overlays = [
     ######### lact 0.7.3 PR until 25.05
     # https://github.com/NixOS/nixpkgs/pull/374771
-    (final: prev: {
-      lact = final.callPackage "${lact-src}/pkgs/by-name/la/lact/package.nix" {
-        hwdata = final.callPackage "${lact-src}/pkgs/by-name/hw/hwdata/package.nix" { };
-      };
-    })
+    # (final: prev: {
+    #   lact = final.callPackage "${lact-src}/pkgs/by-name/la/lact/package.nix" {
+    #     hwdata = final.callPackage "${lact-src}/pkgs/by-name/hw/hwdata/package.nix" { };
+    #   };
+    # })
     #########
   ];
 }
