caa103b77ce28f6178431b2ab24fcbb9e2d09609
andi242 <andi242@gmail.com>
2025-06-21 08:29:25 +0200

    up/gnome

--- modules/home/home-pc.nix
+++ modules/home/home-pc.nix
@@ -9,8 +9,18 @@
     zellij.enable = true;
     userPkgs = with pkgs; [
       nvtopPackages.amd
-      # ytmdesktop
       streamcontroller
+      # (streamcontroller.overrideAttrs (oldAttrs: rec {
+      #   version = "1.5.0-beta.9";
+      #   src = pkgs.fetchFromGitHub {
+      #     repo = "StreamController";
+      #     owner = "StreamController";
+      #     # https://github.com/StreamController/StreamController/releases/
+      #     # get hash from releases
+      #     rev = "be88bad807d66c3595f19f778bf92904951919e8";
+      #     hash = "sha256-CFJFq5DdK4nZnFs8EATbp1kw47p3NIGfUrwlEa0+Ogs=";
+      #   };
+      # }))
       (discord.override {
         withOpenASAR = true;
         # withVencord = true; # can do this here too
