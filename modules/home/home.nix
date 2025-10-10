044e254e1e6cbe6d8adda1018578f281a9181711
andi242 <andi242@gmail.com>
2025-10-10 23:03:18 +0200

    rm nixvim

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -9,13 +9,13 @@
     homeDirectory = "/home/ad";
     stateVersion = "24.11";
     packages = with pkgs; [
-      inputs.nixvim.packages.${system}.default
+      # inputs.nixvim.packages.${system}.default
       obsidian
       joplin-desktop
     ];
-    file = {
-      ".config/nvim/snippets/nix.json".source = ./modules/misc/snippets-nix.json;
-    };
+    # file = {
+    #   ".config/nvim/snippets/nix.json".source = ./modules/misc/snippets-nix.json;
+    # };
     sessionVariables = {
       # NH_FLAKE = "/home/ad/gits/nix";
       NIXOS_OZONE_WL = "1";
